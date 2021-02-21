a37997b7f0e73b555002e2c22e8bb722fbbf4d25
Michel Palourdio <mpalourdio@gmail.com>
2021-02-21 22:35:58 +0100

    chore(deprecated): Fix deprecated API

--- src/main/kotlin/com/mpalourdio/projects/springbootkotlinangular/api/ApiController.kt
+++ src/main/kotlin/com/mpalourdio/projects/springbootkotlinangular/api/ApiController.kt
@@ -12,7 +12,6 @@ package com.mpalourdio.projects.springbootkotlinangular.api
 import com.mpalourdio.projects.springbootkotlinangular.config.IGNORED_PATH
 import org.apache.commons.lang3.StringUtils
 import org.springframework.boot.autoconfigure.web.ServerProperties
-import org.springframework.core.ParameterizedTypeReference
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.reactive.function.client.WebClient
@@ -24,10 +23,10 @@ import java.util.*
 class ApiController(private val serverProperties: ServerProperties) {
 
     private val webClient: WebClient = WebClient.create(
-            "http://localhost:"
-                    + serverProperties.port
-                    + StringUtils.stripToEmpty(serverProperties.servlet.contextPath)
-                    + IGNORED_PATH
+        "http://localhost:"
+                + serverProperties.port
+                + StringUtils.stripToEmpty(serverProperties.servlet.contextPath)
+                + IGNORED_PATH
     )
 
     @PostMapping(path = ["/fast"])
@@ -41,15 +40,11 @@ class ApiController(private val serverProperties: ServerProperties) {
     @GetMapping(path = ["/slow-but-reactive"])
     fun slowButReactive(): Mono<ResponseEntity<List<String>>> {
         return webClient
-                .get()
-                .uri("/slow")
-                .exchange()
-                .flatMap { r ->
-                    r.toEntity(object : ParameterizedTypeReference<List<String>>() {
-
-                    })
-                }
-                .onErrorReturn(ResponseEntity.ok(ArrayList()))
+            .get()
+            .uri("/slow")
+            .retrieve()
+            .toEntityList(String::class.java)
+            .onErrorReturn(ResponseEntity.ok(ArrayList<String>()));
     }
 
     @CrossOrigin(origins = ["http://localhost:4200"], allowedHeaders = ["x-requested-with"])
@@ -57,8 +52,10 @@ class ApiController(private val serverProperties: ServerProperties) {
     fun slow(): ResponseEntity<List<String>> {
         Thread.sleep(3000)
         val results = ArrayList<String>()
-        results.add("Hey, I am the slow cross-origin response "
-                + "(if performed from a port different from " + serverProperties.port + ")")
+        results.add(
+            "Hey, I am the slow cross-origin response "
+                    + "(if performed from a port different from " + serverProperties.port + ")"
+        )
 
         return ResponseEntity.ok(results)
     }
